648bed9c68d499349fcab24226e874eea7d375a1
shakil-1318 <shakiljust21@gmail.com>
2023-07-24 23:12:52 +0600

    testimonial add

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Testimonial Slider
+// Slider owl carousel
+jQuery(document).ready(function ($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    // autoplay: true,
+    dots: true,
+    autoplayTimeout: 4500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1170: {
+        items: 3
+      }
+    }
+  });
+});
