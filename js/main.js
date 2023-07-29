fc2273a3d38471269f753e04c6a6bf9c6604724b
shakil-1318 <shakiljust21@gmail.com>
2023-07-30 01:53:04 +0600

    some fix

--- js/main.js
+++ js/main.js
@@ -25,3 +25,14 @@ jQuery(document).ready(function ($) {
     }
   });
 });
+
+// Scroll back to top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
