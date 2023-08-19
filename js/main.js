928aa3a17d0260cd2a27e02ec3f1d98e71180ba5
shakil-1318 <shakiljust21@gmail.com>
2023-08-20 02:05:55 +0600

    about page add

--- js/main.js
+++ js/main.js
@@ -28,6 +28,19 @@ jQuery(document).ready(function ($) {
   });
 });
 
+// Video Popup
+$(document).ready(function () {
+  $('.popup-youtube').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    // removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+});
+
+
 
 
 
