e3fcc85b5f39ed746c70f6582ac85b2d82f667ad
shakil-1318 <shakiljust21@gmail.com>
2023-09-14 21:28:25 +0600

    update

--- js/main.js
+++ js/main.js
@@ -68,44 +68,6 @@ $(document).ready(function () {
 });
 
 
-
-
-
-// Scroll back to top
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-})
-
 // Footer Current Year
 var year = document.getElementById('currentYear');
 year.innerHTML = new Date().getFullYear();
-
-
-// $(document).ready(function () {
-//   $(window).scroll(function (event) {
-//     var scroll = $(window).scrollTop();
-//     if (scroll > 100) {
-//       $("#demo").show();
-//     } else {
-//       $("#demo").hide();
-//     }
-//     // Do something
-//   });
-//   $(window).on("load", function () {
-//     var windowSize = $(window).width();
-//     if (windowSize < 481) {
-//       $(".data-section").addClass("mobileView");
-//     } else {
-//       $(".data-section").addClass("webView");
-//     }
-//   });
-
-
-
-// });
