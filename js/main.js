f1d53fbc313dbbea641f54195cbb897d6e63355c
shakil-1318 <shakiljust21@gmail.com>
2023-09-14 18:11:13 +0600

    update package

--- js/main.js
+++ js/main.js
@@ -5,6 +5,33 @@
 jQuery(document).ready(function ($) {
   "use strict";
   //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-package-mobile').owlCarousel({
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 4500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1170: {
+        items: 1
+      }
+    }
+  });
+});
+
+// Slider owl carousel
+jQuery(document).ready(function ($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
   $('#customers-testimonials').owlCarousel({
     loop: true,
     center: true,
